a23e1240c1d7ca6f3f77ac99591468a6a7dc0166
Adel <adel.ngo@yahoo.com>
2023-08-14 23:34:00 -0500

    Complete Step 7

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Created by Adel Ngo
 
-This is a survey application that uses some of the Flask tools.
+This is a survey application that uses Flask tools like Jinja, Flash messages and the Flask Toolbar debugger.
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import *
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 
@@ -9,26 +9,44 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
 # keep track of questions
-quest_length = len(satisfaction_survey.questions)
+quest_length = len(survey.questions)
 responses = []
 
 
 @app.route("/")
 def home_page():
-    """The root page"""
+    """The home page"""
+
     return render_template(
         "root.html",
-        title=satisfaction_survey.title,
-        steps=satisfaction_survey.instructions,
+        title=survey.title,
+        steps=survey.instructions,
     )
+
+
+@app.route("/begin")
+def start_survey():
+    """Start survey"""
+
+    return redirect("/questions/0")
 
 
 @app.route("/questions/<int:quest_num>")
 def question(quest_num):
-    """Question Views"""
-    question = satisfaction_survey.questions[quest_num].question
-    choice1 = satisfaction_survey.questions[quest_num].choices[0].replace(" ", "_")
-    choice2 = satisfaction_survey.questions[quest_num].choices[1].replace(" ", "_")
+    """Display Questions"""
+
+    if len(responses) != quest_num:
+        # User attempts to access questions out of order
+        flash(f"Invalid Question #:{quest_num}")
+        return redirect(f"/questions/{len(responses)}")
+
+    if quest_num >= quest_length:
+        # User has answered all the questions
+        return redirect("/thanks")
+
+    question = survey.questions[quest_num].question
+    choice1 = survey.questions[quest_num].choices[0].replace(" ", "_")
+    choice2 = survey.questions[quest_num].choices[1].replace(" ", "_")
     return render_template(
         "question.html",
         question=question,
@@ -40,12 +58,16 @@ def question(quest_num):
 
 @app.route("/answer/<int:quest_num>", methods=["POST"])
 def answer(quest_num):
-    """POST request then redirect to next question"""
+    """Save response then redirect to next question"""
+
+    # Increment quest_num to go to next question
     quest_num += 1
     choice = request.form.get("choice")
     responses.append(choice)
-    flash(f"choice was {responses[quest_num-1]}")
+    flash(f"Choice was {responses[quest_num-1]}")
+
     if quest_num >= quest_length:
+        # User has answered all the questions
         return redirect("/thanks")
     else:
         return redirect(f"/questions/{quest_num}")
